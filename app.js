19eadaaf5df953dbe335cba8aa76f2020c6bb1bd
Philip <philipmboyah@gmail.com>
2022-05-15 10:30:53 +0000

    Touch made on files

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const app = require('./public/index');
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
 
-new app().initializeapp();
+app.get("/", function(req,res){
+  res.sendFile(__dirname, + "/public/index.html");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on Port 3000");
+})
--- routes/apiRoutes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const animalRoutes = require('../apiRoutes/animalRoutes');
-
-router.use(animalRoutes);
-router.use(require('./zookeeperRoute'));
-
-module.exports = router;
